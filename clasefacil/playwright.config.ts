fef7db8f98378d930d6e7b949e6814476c5a32b2
evelyn villarreal <evelyn.villarreal245@gmail.com>
2025-11-06 20:14:23 -0300

    usando v0

--- clasefacil/playwright.config.ts
+++ clasefacil/playwright.config.ts
@@ -71,11 +71,9 @@ export default defineConfig({
   ],
 
   /* Run your local dev server before starting the tests */
-  webServer: {
-    command: 'npm run dev',
-    port: 3000,
-    timeout: 120 * 1000, // Tiempo máximo de espera
-    reuseExistingServer: !process.env.CI,
-  },
-
+  // webServer: {
+  //   command: 'npm run start',
+  //   url: 'http://localhost:3000',
+  //   reuseExistingServer: !process.env.CI,
+  // },
 });
