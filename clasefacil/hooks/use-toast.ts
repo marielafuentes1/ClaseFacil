1ce3458af38b319f037bf34bc22b37e6879b6497
evelyn villarreal <evelyn.villarreal245@gmail.com>
2025-11-12 16:04:22 -0300

    creando las tablas en chema.prisma

--- clasefacil/hooks/use-toast.ts
+++ clasefacil/hooks/use-toast.ts
@@ -1,5 +1,10 @@
 export function useToast() {
-    return {
-        toast: (msg: string) => alert(msg)
-    };
+  return {
+    toast: (props: { title: string; description?: string; variant?: string }) => {
+      let message = `${props.title}`;
+      if (props.description) message += `\n${props.description}`;
+      if (props.variant === "destructive") message = "❌ " + message;
+      alert(message);
+    },
+  };
 }
